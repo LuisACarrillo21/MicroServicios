c2bd593dbda545a8b608e02da1c3e3159efde154
Luis Angel Carrillo <135722083+LuisACarrillo21@users.noreply.github.com>
2026-06-21 04:57:20 -0600

    Update ServerDuda.java

--- microservicio/ServerDuda.java
+++ microservicio/ServerDuda.java
@@ -2,6 +2,7 @@ package microservicio;
 
 public class ServerDuda {
     public static void main(String[] args) throws Exception {
-        new AsuntoServicio(5103, Estructura.Asunto.DUDA, 10).start();
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "5103"));
+        new AsuntoServicio(port, Estructura.Asunto.DUDA, 10).start();
     }
 }
