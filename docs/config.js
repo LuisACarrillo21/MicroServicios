abd77e7eefd4fc22bcd3eee90c427f626b0304b1
Luis Angel Carrillo <135722083+LuisACarrillo21@users.noreply.github.com>
2026-06-22 20:25:32 -0600

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -3,5 +3,5 @@
 window.APP_CONFIG = {
   //API_BASE: "https://ca-gateway-sd.calmcoast-a44b42e6.canadacentral.azurecontainerapps.io"
   //API_BASE: "https://cr-gateway-sd-850678681976.us-central1.run.app"
-  API_BASE: "http://3.92.88.69:8080"
+  API_BASE: "https://gateway-aws-practica10.duckdns.org"
 };
