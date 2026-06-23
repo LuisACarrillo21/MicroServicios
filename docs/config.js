f6fba1e92208c177d0dcb3f9554fc14cba1ba005
Luis Angel Carrillo <135722083+LuisACarrillo21@users.noreply.github.com>
2026-06-22 21:12:52 -0600

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,7 @@
 // Si lo dejas vacío, la PWA funciona offline
 // Para usar backend local: "http://localhost:8080"
 window.APP_CONFIG = {
-  //API_BASE: "https://ca-gateway-sd.calmcoast-a44b42e6.canadacentral.azurecontainerapps.io"
-  //API_BASE: "https://cr-gateway-sd-850678681976.us-central1.run.app"
-  API_BASE: "https://gateway-aws-practica10.duckdns.org"
+  //API_BASE: "https://ca-gateway-sd.calmcoast-a44b42e6.canadacentral.azurecontainerapps.io" //Azure
+  //API_BASE: "https://cr-gateway-sd-850678681976.us-central1.run.app" //cloud
+  API_BASE: "https://gateway-aws-practica10.duckdns.org" //AWS
 };
